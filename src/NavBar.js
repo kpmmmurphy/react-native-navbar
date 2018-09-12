4c1dc430d4c4ee4219d4c7a9b11c6107d1bfe94d
Kevin Murphy <kpmmmurphy@gmail.com>
2018-09-12 14:17:11 +0100

    Updated to new Prop-Types dependency

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,4 +1,5 @@
-import React, { Component, PropTypes } from 'react'
+import React, { Component } from 'react'
+import PropTypes from 'prop-types';
 import { Navigator } from 'react-native-deprecated-custom-components'
 import { isSameUrl, findRoute } from './routing'
 import routeMapper from './route-mapper'
